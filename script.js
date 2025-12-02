7f4036b64411a0480ab8b1ff4592ea6c579c7544
Bohdan Sydorenko <163444640+Sagmanua@users.noreply.github.com>
2025-12-02 18:49:43 +0100

    version0.1

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Load portfolio data from JSON
 fetch('data.json')
   .then(response => response.json())
   .then(items => {
@@ -8,12 +7,22 @@ fetch('data.json')
       const card = document.createElement('div');
       card.className = 'card';
 
-      card.innerHTML = `
-        <div class="title">${item.title}</div>
-        <div class="desc">${item.description}</div>
-        <div class="desc">${item.language}</div>
-        <div class="desc"><a href="${item.linkgithub}">link a github</a> </div>
-      `;
+      let mediaHTML = "";
+
+      // If an image exists:
+
+    if (item.image) {
+    mediaHTML = `<img src="${item.image}" class="thumb" alt="${item.title}">`;
+    }
+
+  card.innerHTML = `
+      <div class="title">${item.title}</div>
+      ${mediaHTML}
+      <div class="desc">${item.description}</div>
+      <div class="desc"><p>La Language que usa: ${item.language}</div>
+      <div class="desc"><a href="${item.github}">GitHub Link</a></div>
+    `;
+
 
       grid.appendChild(card);
     });
